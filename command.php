82f8323c3507e7c18a483b0e96e96cde697850d5
anthonylbm69 <antonex.ly@gmailcom>
2025-03-25 12:07:59 +0100

    gestion de commande

--- command.php
+++ command.php
@@ -1,1 +1,9 @@
 <?php
+if (isset($_GET['ip'])){
+    $ip = $_GET['ip'];
+
+    $command = shell_exec("ping -c 1" . $ip);
+    echo "<pre>" . $command . "</pre>";
+} else {
+    echo "Veuillez fournir une IP";
+}
