532fdb4cd04fedece43e2f0ebb73d08603c6ce08
anthonylbm69 <antonex.ly@gmailcom>
2025-03-25 17:13:30 +0100

    change name db login

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-$pdo = new PDO("mysql:host=172.16.30.129;dbname=injectione;", "root", "secret", [
+$pdo = new PDO("mysql:host=172.16.30.129;dbname=injection;", "root", "secret", [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 
