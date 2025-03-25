d30227d18b7f8f94490a802fa6b457b652382260
anthonylbm69 <antonex.ly@gmailcom>
2025-03-25 15:26:00 +0100

    correction faille ping

--- command.php
+++ command.php
@@ -1,6 +1,7 @@
 <?php
 if (isset($_GET['ip'])){
-    $ip = $_GET['ip'];
+    //$ip = $_GET['ip'];
+    $ip = escapeshellarg($_GET['ip']);
 
     $command = shell_exec("ping -c 1 " . $ip);
     echo "<pre>" . $command . "</pre>";
