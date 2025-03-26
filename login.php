165ed70078f4bcee8adfa75d6272bcd3959e78de
anthonylbm69 <antonex.ly@gmailcom>
2025-03-26 11:00:42 +0100

    edit db ip

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO("mysql:host=192.168.194.163;dbname=injection", "root", "secret", [
+$pdo = new PDO("mysql:host=172.16.30.129;dbname=injection", "root", "secret", [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 
@@ -20,7 +20,7 @@ if ($resultat) {
     $_SESSION['id'] = $resultat['id'];
     $_SESSION['username'] = $username;
 
-    echo "Connexion réussie !";
+    header('Location: home.php');
 
 } else {
     echo "Mot de passe incorrect !";
